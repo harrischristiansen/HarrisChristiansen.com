6271e7bdb06608d9ba82cf80e9e48aec9bbb46ea
Harris Christiansen (hc15ret) <harrischristiansen@mac.com>
2015-01-13 20:06:11 -0500

    Added StackOverflow Link

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
 					<li><a href="https://plus.google.com/101657892778732134864" class="icon fa-google-plus" target="_blank"><span class="label">Google+</span></a></li>
 					<li><a href="https://twitter.com/harriswchris" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
 					<li><a href="https://github.com/harrischristiansen" class="icon fa-github" target="_blank"><span class="label">Github</span></a></li>
+					<li><a href="http://stackoverflow.com/users/3884972" class="icon fa-stack-overflow" target="_blank"><span class="label">StackOverflow</span></a></li>
 					<li><a href="https://www.linkedin.com/in/harrischristiansen" class="icon fa-linkedin" target="_blank"><span class="label">LinkedIn</span></a></li>
 					<li><a href="https://angel.co/harrischristiansen" class="icon fa-angellist" target="_blank"><span class="label">AngelList</span></a></li>
 					<li><a href="mailto:harris@harrischristiansen.com" class="icon fa-envelope"><span class="label">Email</span></a></li>
